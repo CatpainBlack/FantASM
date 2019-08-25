c7b682f7ccd4f9245d6bd25469f550b0c888216f
Captain Black <captainblack@zx-spectrum.org.uk>
2019-08-25 11:16:02 +0100

    Fixed error messages for second pass

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -15,52 +15,54 @@ mod expression_impl;
 
 //#[derive(Debug)]
 struct TokenReader<R> {
-    reader: R,
-    operators: String,
-    delimiters: String,
-    line_number: isize,
-    words: Vec<String>,
-    token_string: String,
-    tokens: Vec<Token>,
-    preceding_token: Token,
+	reader: R,
+	operators: String,
+	delimiters: String,
+	line_number: isize,
+	words: Vec<String>,
+	token_string: String,
+	tokens: Vec<Token>,
+	preceding_token: Token,
 }
 
 #[derive(Debug)]
 pub struct ForwardReference {
-    is_expression: bool,
-    pc: isize,
-    label: String,
-    expression: Vec<Token>,
-    is_relative: bool,
-    byte_count: isize,
+	is_expression: bool,
+	pc: isize,
+	label: String,
+	expression: Vec<Token>,
+	is_relative: bool,
+	byte_count: isize,
+	line_no: isize,
+	file_name: String,
 }
 
 
 pub struct Assembler {
-    line_number: Vec<isize>,
-    tokens: Vec<Token>,
-    origin: isize,
-    current_pc: isize,
-    labels: HashMap<String, isize>,
-    constants: HashMap<String, isize>,
-    forward_references: Vec<ForwardReference>,
-    bytes: Vec<u8>,
+	line_number: Vec<isize>,
+	tokens: Vec<Token>,
+	origin: isize,
+	current_pc: isize,
+	labels: HashMap<String, isize>,
+	constants: HashMap<String, isize>,
+	forward_references: Vec<ForwardReference>,
+	bytes: Vec<u8>,
     file_name: Vec<String>,
-    console_output: bool,
-    total_lines: isize,
-    expr: ExpressionParser,
-    z80n_enabled: bool,
-    cspect_enabled: bool,
+	console_output: bool,
+	total_lines: isize,
+	expr: ExpressionParser,
+	z80n_enabled: bool,
+	cspect_enabled: bool,
 }
 
 #[derive(Debug)]
 pub enum ErrorLevel {
-    Fatal,
+	Fatal,
 }
 
 pub struct Error {
-    pub line_no: isize,
-    pub message: String,
-    pub level: ErrorLevel,
-    pub file_name: String,
+	pub line_no: isize,
+	pub message: String,
+	pub level: ErrorLevel,
+	pub file_name: String,
 }
