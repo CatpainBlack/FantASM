4970e6a26c36870a95af8c5c1328621bb5e2832e
Captain Black <captainblack@zx-spectrum.org.uk>
2019-10-17 10:04:23 +0100

    Fixed (ix+expr)

--- tests/bug.asm
+++ tests/bug.asm
@@ -1,20 +1,5 @@
 	org 32768
 
-const_x = 0
-const_y = 1
-const_z = 0
-
-if bibble = 0
-    if const_y = 0
-        ld hl,1
-    else
-        ld  hl,2
-    endif
-else
-    if const_z = 0
-        ld hl,3
-    else
-        ld  hl,4
-    endif
-endif
+    ld  a,(ix-3+2+1)
+    ld  a,(ix)
 
