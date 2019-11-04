fd999b4302245bc9f291b8e53644432938e2f32b
Captain Black <captainblack@zx-spectrum.org.uk>
2019-11-04 14:04:07 +0000

    Version codename

--- src/options.rs
+++ src/options.rs
@@ -55,7 +55,7 @@ pub struct Options {
 
 impl Options {
     pub fn parse() -> Result<Options, String> {
-        let description = format!("\nFantASM {} (Ankh-Morpork) [{}]\n\u{000A9}2019 Captain Black\n", version!(), env!("BUILD_DATE"));
+        let description = format!("\nFantASM {} (Guy Fawkes) [{}]\n\u{000A9}2019 Captain Black\n", version!(), env!("BUILD_DATE"));
         let mut options = Options::default();
         {
             let mut parser = ArgumentParser::new();
